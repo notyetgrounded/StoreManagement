21465a50f064fcad0432bcacc8f3dca80e627f79
Kishore N A <kkish056@gmail.com>
2023-04-16 18:24:07 +0530

    Added Configuration and Connection String

--- StoreManagement.presentation/Program.cs
+++ StoreManagement.presentation/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using StoreManagement.Repository.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreDbContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("StoreDbConnectionString");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
